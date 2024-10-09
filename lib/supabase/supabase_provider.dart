7c90ad1685529dc2c91f480d21bd08a332e746a9
Ben Schreiber <bpschreiber2003@gmail.com>
2024-10-08 18:27:16 -0700

    rm keys (they're taken down from cloud too)

--- lib/supabase/supabase_provider.dart
+++ lib/supabase/supabase_provider.dart
@@ -15,9 +15,8 @@ late StreamSubscription _authSubscription;
 /// Should be populated before the app is started.
 final clientProviderAsync = FutureProvider<SupabaseClient>((ref) async {
   final supabase = await Supabase.initialize(
-    url: "https://tywqkhiiksnnvtdswlbc.supabase.co",
-    anonKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5d3FraGlpa3NubnZ0ZHN3bGJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI4MDk5NDAsImV4cCI6MjAzODM4NTk0MH0.VSQLswtGZhlrc06k64F47geS6K6_lits3a1Sk4w7akI",
+    url: "",
+    anonKey: "",
   );
 
   final client = supabase.client;
